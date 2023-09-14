1732bfb758ed526809b25c380805c06d62cd464b
aaaansi <theshortrolow@gmail.com>
2023-09-14 18:09:57 +1200

    dead lines

--- UnitTests/main.cpp
+++ UnitTests/main.cpp
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+int another(int param)
+{
+puts("Something");
+if (param == 0)
+	puts("Zero");
+else
+	puts("non Zero");
+
+return 0;
+}
+
 int main(int argc, char *argv[])
 {
     puts("main");
     for (int x = 0; x < 10; x++)
 	    printf("%d\n", x);
+    another(0);
     puts("main fin");
     return 0;
 }
