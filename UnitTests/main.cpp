528e0c1f9c836f331702ee3acd744d559cfbffc6
aaaansi <theshortrolow@gmail.com>
2023-09-14 16:33:42 +1200

    coveage test

--- UnitTests/main.cpp
+++ UnitTests/main.cpp
@@ -1,10 +1,10 @@
-#include <stdio.>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
     puts("main");
     //::testing::InitGoogleTest(&argc, argv);
     //RUN_ALL_TESTS();
-    //puts("main fin");
+    puts("main fin");
     return 0;
 }
