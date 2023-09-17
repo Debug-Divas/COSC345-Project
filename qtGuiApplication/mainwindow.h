0bdc0e66efb5216f2c200e5560dddb1759a4c4a9
List28 <linda.strid98@hotmail.com>
2023-09-17 23:19:16 +0200

    removed comment

--- qtGuiApplication/mainwindow.h
+++ qtGuiApplication/mainwindow.h
@@ -1,7 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-//#include <QMainWindow>
+#include <QMainWindow>
 #include <vector> // Include the <vector> header
 
 // Include necessary headers for your classes here
